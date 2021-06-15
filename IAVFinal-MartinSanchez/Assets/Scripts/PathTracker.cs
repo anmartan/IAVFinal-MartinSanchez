c2eacdadfb848a31a7aeb6dd43f0dd0d5c71c219
anmartan <anmartan@ucm.es>
2021-06-15 13:55:57 +0200

    ADDED: Tremaux's Algorithm
    Moves randomly but takes into account the places it's been to before.
    When there is no exit, the character remains still, as it cannot go anywhere else.

---
+++ IAVFinal-MartinSanchez/Assets/Scripts/PathTracker.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Saves some data about the path calculated, such as the number of tiles the character has walked on, or the time since the simulation started
+/// Shows a map with information about which tiles have been walked on, in a graphic way.
+/// </summary>
+public class PathTracker : MonoBehaviour
+{
+    [SerializeField] private GameObject cellPrefab_;
+    [SerializeField] private Transform parent_;
+    [SerializeField] private Color unvisited_;          // the color used to paint the visited tiles
+    [SerializeField] private Color visited_;            // the color used to paint the visited tiles
+    [SerializeField] private float alpha_;              // how much less transparency a tile will have if the character walks over it
+
+    private GameObject[,] map_;     // the cells that will be painted over the map
+
+    private int[,] valuesMap_;      // how many times a tile has been walked over
+    private level level_;           // info about the maze
+
+    /// <summary>
+    /// Creates the tiles that will draw the path Red Riding Hood is following.
+    /// </summary>
+    public void CreateMap()
+    {
+        level_ = GameManager.instance().GetLevel();
+        map_ = new GameObject[level_.size_, level_.size_];
+        valuesMap_ = new int[level_.size_, level_.size_];
+
+
+        for (int i = 0; i < level_.size_; i++)
+        {
+            for (int j = 0; j < level_.size_; j++)
+            {
+                valuesMap_[i, j] = 0;
+
+                Vector3 pos = new Vector3((j + 0.5f) * MazeCreator.WORLD_SCALE, 0.1f, -(i + 0.5f) * MazeCreator.WORLD_SCALE);
+                map_[i,j] = Instantiate(cellPrefab_, pos, Quaternion.identity, parent_);
+                map_[i, j].GetComponent<MeshRenderer>().material.color = unvisited_;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Changes the alpha of the tile to show the path. 
+    /// Updates the valuesMap_.
+    /// </summary>
+    /// <param name="cell">The tile the character has stepped into.</param>
+    /// <returns>The ammount of times the character has walked over this tile.</returns>
+    public int VisitCell(Vector2 cell)
+    {
+        float alpha = map_[(int)cell.x, (int)cell.y].GetComponent<MeshRenderer>().material.color.a + alpha_;
+        Color color = visited_;
+        color.a = Mathf.Min(alpha, 1);  // so it does not go over 1
+
+        map_[(int)cell.x, (int)cell.y].GetComponent<MeshRenderer>().material.color = color;
+
+        return valuesMap_[(int)cell.x, (int)cell.y];
+    }
+}
---
+++ IAVFinal-MartinSanchez/Assets/Scripts/SolverAlgorithms/TremauxSolver.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Improvement of the random solver: in every intersection, chooses a random direction to follow.
+/// Remembers where it has already been, so it doesnt repeat the same path over and over again.
+/// This is known as the Tremaux algorithm
+/// </summary>
+public class TremauxSolver : Solver
+{
+    /// <summary>
+    /// The path chosen depends on how many times a road has been already used.
+    /// This indicates if a road will be chosen for the first time, if its been walked over once, or if it is useless to choose it again
+    /// </summary>
+    private enum STATE {NOT_CROSSED = 0, CROSSED, FORBIDDEN};
+
+    private STATE[,] map_;       // representation of the intersections in the scene
+
+
+    /// <summary>
+    /// First rule: dont go into a corridor that has two marks on it (FORBIDDEN).
+    /// Second rule: if you step into a corridor with a mark on it (CROSSED), go back.
+    /// Third rule: if youre supposed to go back but you cant because theres two marks on that corridor,
+    /// go into the hallway with the least marks on it (NOT_CROSSED or CROSSED).
+    /// </summary>
+    override protected void FindPath(Vector2 playerPos)
+    {
+        // if it cannot go anywhere else, it goes back again
+        if (possibleDirections_.Count == 0)
+        {
+            direction_ = -transform.forward;
+            return;
+        }
+        
+        //if it is at a corner and can only go one way, it does so
+        if(possibleDirections_.Count == 1)
+        {
+            direction_ = possibleDirections_[0];
+            return;
+        }
+
+        //---------- Tremaux algorithm starts here ----------//
+
+        // marks the path it comes from
+        Vector2 from = new Vector2(Mathf.RoundToInt(transform.forward.z), 
+                                  -Mathf.RoundToInt(transform.forward.x));
+        VisitRoad(playerPos + from);
+
+        // checks for unmarked paths
+        Vector3 dir = CheckState(playerPos, STATE.NOT_CROSSED);
+
+        // if there are not unmarked paths, checks for those with one mark
+        bool goback = false;
+        if (dir == Vector3.zero) 
+        {
+            dir = CheckState(playerPos, STATE.CROSSED);
+            goback = true;
+        }
+
+        // if there are not marked paths, every turn is forbidden. There is no exit
+        if (dir == Vector3.zero)
+        {
+            Debug.Log("No exit");
+            direction_ = Vector3.zero;
+            return;
+        }
+
+        Vector2 to = new Vector2(-Mathf.RoundToInt(dir.z),
+                                Mathf.RoundToInt(dir.x));
+
+        // if its supposed to go back, two things can happen
+        if (goback)
+        {
+            // if it cant go back because its forbidden, it takes another direction
+            if (GetState(playerPos + from) == STATE.FORBIDDEN)
+            {
+                VisitRoad(playerPos + to);
+                direction_ = dir;
+            }
+            // if it can go back, it does
+            else
+            {
+                VisitRoad(playerPos + from);
+                direction_ = -transform.forward;
+            }
+        }
+        // it goes in the direction calculated
+        else
+        {
+            VisitRoad(playerPos + to);
+            direction_ = dir;
+        }
+    }
+
+
+    private Vector3 CheckState(Vector2 playerPos, STATE state)
+    {
+        Vector3 direction = Vector3.zero;
+
+        // checks if there is any unmarked path, starting at a random direction
+        int i = Random.Range(0, possibleDirections_.Count);
+        int loops = 0;
+        bool found = false;
+        while (loops < possibleDirections_.Count && !found)
+        {
+            Vector2 dir = new Vector2(-Mathf.RoundToInt(possibleDirections_[i].z),
+                                        Mathf.RoundToInt(possibleDirections_[i].x));
+            found = map_[(int)(playerPos.x + dir.x), (int)(playerPos.y + dir.y)] == state;
+
+            if (!found)
+            {
+                i = (i + 1) % possibleDirections_.Count;
+                loops++;
+            }
+            else
+                direction = possibleDirections_[i];
+        }
+
+        return direction;
+    }
+
+    /// <summary>
+    /// Returns the state of the road given.
+    /// </summary>
+    /// <param name="pos">The position of the intersection</param>
+    /// <returns>The state of the road in that direction</returns>
+    private STATE GetState(Vector2 pos)
+    {
+        return map_[(int)pos.x, (int)pos.y];
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <param name="dir"></param>
+    private void VisitRoad(Vector2 pos)
+    {
+        if (map_[(int)pos.x, (int)pos.y] < STATE.FORBIDDEN)
+            map_[(int)pos.x, (int)pos.y]++;
+    }
+
+
+    //------ Unity functions. All of them call the base method, defined in Solver.cs ------//
+
+    protected override void Start()
+    {
+        base.Start();
+        map_ = new STATE[level_.size_, level_.size_];
+    }
+    protected override void Update()
+    {
+        base.Update();
+    }
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+    }
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+    }
+}
